a24b9bc8caecf0f24b20c5091874863331882997
Meck <yesmeck@gmail.com>
2012-11-22 00:10:30 +0800

    修正下载数量不准确的问题

--- bin/star.rb
+++ bin/star.rb
@@ -3,7 +3,7 @@
 
 require "star"
 
-downloaded_song = []
+@downloaded_song = []
 
 star = Star.new
 
@@ -15,20 +15,26 @@ puts "Captcha:"
 puts star.captcha
 captcha = gets.chomp
 puts "How many songs do you want?"
-download_count = gets.chomp
+@download_count = gets.chomp
 puts "Download to?"
 save_path = gets.chomp
 
 star.login(username, password, captcha)
 
-while downloaded_song.count < download_count.to_i
+
+def download_enough?
+  @downloaded_song.count >= @download_count.to_i
+end
+
+while !download_enough?
   star.songs.each do |song|
-    if !downloaded_song.include?(song.sid)
+    if !@downloaded_song.include?(song.sid) && !download_enough?
       puts "Downloading 《#{song.title} - #{song.artist}》..."
       song.save_to(save_path)
-      downloaded_song << song.sid
+      @downloaded_song << song.sid
     end
   end
 end
 
-puts "Downloaded #{downloaded_song.count} songs."
+puts "Downloaded #{@downloaded_song.count} songs."
+
